ca0ccd4464acab1f117be687bd0103baf29a023d
dotslashapaar <apaarsaini007@gmail.com>
2026-07-01 00:24:11 +0530

    added routes

--- src/routes/shortner.rs
+++ src/routes/shortner.rs
@@ -1,1 +1,12 @@
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
+use crate::handlers::shortner::{AppState, create_link_handler, redirect_handler};
+
+pub fn shortner_routes() -> Router<AppState> {
+    Router::new()
+        .route("/shorten", post(create_link_handler))
+        .route("/{slug}", get(redirect_handler))
+}
